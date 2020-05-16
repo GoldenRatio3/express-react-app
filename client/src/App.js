80291b6c6f314ccd90539c849c5bd65ae0a74612
Brad Winter <bradleyswinter@hotmail.co.uk>
2020-05-16 14:43:07 +0100

    Added custom greeting input

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import TextField from "@material-ui/core/TextField";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = {
+  textInput: {
+    color: "white",
+  },
+};
 
 class App extends Component {
   constructor() {
@@ -9,7 +17,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("/api/greeting?name=test").then((resp) => {
+    fetch("/api/greeting").then((resp) => {
       if (resp.status !== 200) {
         console.log("Looks like there was a problem." + resp.status);
       }
@@ -20,24 +28,40 @@ class App extends Component {
     });
   }
 
+  fetchGreeting = (event) => {
+    const name = event.target.value;
+    fetch("/api/greeting?name=" + name).then((resp) => {
+      if (resp.status !== 200) {
+        console.log("Looks like there was a problem." + resp.status);
+      }
+
+      return resp.json().then((data) => {
+        this.setState({ greeting: data.greeting });
+      });
+    });
+  };
+
   render() {
+    const { classes } = this.props;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>{this.state.greeting}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <TextField
+            InputProps={{
+              className: classes.textInput,
+            }}
+            InputLabelProps={{
+              className: classes.textInput,
+            }}
+            label="Enter your name"
+            onBlur={this.fetchGreeting}
+          />
         </header>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
